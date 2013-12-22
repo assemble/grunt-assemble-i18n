781d93bb3f712edc8bbf10416e24728d7f74afb6
Jon Schlinkert <dev@sellside.com>
2013-12-22 00:52:56 -0500

    another examples using pages array. i18n helper is currently limited by the requirement that data to be formatted as a string.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,8 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
 
-    i18n: grunt.file.readJSON('data/i18n.json'),
+    i18n:     grunt.file.readJSON('data/i18n.json'),
+    i18n_alt: grunt.file.readJSON('data/i18n-alt.json'),
 
     assemble: {
       options: {
@@ -22,11 +23,18 @@ module.exports = function(grunt) {
       },
       i18n: {
         options: {
-          language: 'fr',
+          language: 'en',
           pages: '<%= i18n.languages %>'
         },
-        files: {'_demo/': ['*.hbs']},
+        files: {'_demo/i18n/': ['*.hbs']},
       },
+      i18n_alt: {
+        options: {
+          language: 'fr',
+          pages: '<%= i18n_alt.languages %>'
+        },
+        files: {'_demo/i18n-alt/': ['*.hbs']},
+      }
     },
     // Before creating new files, remove files from previous build.
     clean: ['_demo/**/*.html']
