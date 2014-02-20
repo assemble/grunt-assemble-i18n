e482f65c37d44343b3bbafa34e5510a68f6a7f96
Jon Schlinkert <dev@sellside.com>
2014-02-20 09:32:02 -0500

    Merge branch 'interactive-pioneers/lodash-front-matter-rendering-fix'
    Conflicts:
    	Gruntfile.js
    	_demo/i18n-alt/index.html
    	_demo/i18n/index-en.html
    	_demo/i18n/index-es.html
    	_demo/i18n/index-fr.html
    	lib/i18n.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(grunt) {
-  var pages = require('./lib/i18n')(grunt);
+  var pages = require('./lib/i18n');
 
   var read = function(src) {
     return grunt.file.readJSON(src);
@@ -27,14 +27,22 @@ module.exports = function(grunt) {
         layoutdir: 'templates/layouts',
         layout: 'default.hbs',
       },
-      i18n: {
+      i18n_1: {
         options: {
           pages: pages('data/i18n.json')
         },
         dest: '_demo/i18n/',
         src: '!*.*'
       },
-      i18n_alt: {
+      i18n_2: {
+        options: {
+          // Second param for passing file patterns
+          pages: pages(['data/i18n.json'], {patterns: ['**.hbs']})
+        },
+        dest: '_demo/i18n/',
+        src: '!*.*'
+      },
+      i18n_3: {
         options: {
           language: 'fr',
           pages: '<%= i18n_alt.languages %>'
